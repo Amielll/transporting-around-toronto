0fe91c3d213c412dfc043382843eab43426d84be
Amiel Nurja <amielnurja@gmail.com>
2025-03-10 02:28:25 -0400

    again

--- functions/python-app.py
+++ functions/python-app.py
@@ -6,12 +6,13 @@ import requests
 
 app = func.FunctionApp()
 
+@app.function_name(name="main")
 @app.blob_output(arg_name="outputblob",
                 path="stationsblob/{datetime:yyyyMMddHHmm}.json",
                 connection="AzureWebJobsStorage")
 @app.timer_trigger(schedule="0 0 */6 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=True) 
-def timer_trigger(myTimer: func.TimerRequest, outputblob: func.Out[str]) -> None:
+def main(myTimer: func.TimerRequest, outputblob: func.Out[str]) -> None:
     
     station_status_url = "https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_status"
     logging.info("Function triggered, calling station status URL")
