056d3f9956dc66beeda9c470d87da942fecefd61
Amiel Nurja <amielnurja@gmail.com>
2025-03-12 18:07:03 -0400

    fixed deployment of station status scheduler (no longer using actions)

--- functions/function_app.py
+++ functions/function_app.py
@@ -1,17 +1,16 @@
 import azure.functions as func
-import datetime
 import json
 import logging
 import requests
 
 app = func.FunctionApp()
 
-@app.function_name(name="StationStatusScheduler")
-@app.timer_trigger(schedule="0 0 */6 * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=True)
+@app.function_name(name="main")
 @app.blob_output(arg_name="outputblob",
-                 path="stationsblob/{datetime:yyyyMMddHHmm}.json",
-                 connection="AzureWebJobsStorage")
+                path="stationsblob/station_status_{datetime:yyyyMMddHH}.json",
+                connection="AzureWebJobsStorage")
+@app.timer_trigger(schedule="0 0 */6 * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=True) 
 def main(myTimer: func.TimerRequest, outputblob: func.Out[str]) -> None:
     
     station_status_url = "https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_status"
@@ -29,9 +28,3 @@ def main(myTimer: func.TimerRequest, outputblob: func.Out[str]) -> None:
         logging.error(f"GET request failed: {e}")
     except Exception as ex:
         logging.error(f"An error occurred: {ex}")
-
-
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello")
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    return  func.HttpResponse("HttpTrigger1: Hello World!")
