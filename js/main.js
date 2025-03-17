31415961456ce68750fbe5d510934de8a5ada981
dniloy <67499619+dniloy@users.noreply.github.com>
2025-03-16 21:49:19 -0400

    Added montreal bikeshare map

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 import * as d3 from "d3";
 import { BikeshareMapVis } from './mapVis.js';
-let bikeshareMapVis;
+let bikeshareMapVis, montrealBikeshareMapVis;
 
 // TODO: Combine the main.js / neighbourhoodsMain.js load data/init visualization stuff into a single file
 // TODO: Keep the helper functions like changeOpacity or whatever else comes up in respective util files
 let bikesharePromises = [
     d3.json("data/bike_share_stations_2024-01.json"),
     d3.csv("data/bike_share_trips_2024-01.csv"),
-    d3.json('data/Neighbourhoods.geojson')
+    d3.json('data/Neighbourhoods.geojson'),
+    d3.json("data/montreal_station_information.json"),
+    d3.csv("data/DonneesOuvertes2025_0102.csv"),
+    d3.json('data/montreal.geojson')
 ];
 
 Promise.all(bikesharePromises)
@@ -20,4 +23,10 @@ function initProject(allDataArray) {
     let tripData = allDataArray[1];
     let mapData = allDataArray[2];
     bikeshareMapVis = new BikeshareMapVis('bikeshare-map-area', stationData, tripData, mapData);
+
+    let montrealStationData = allDataArray[3];
+    let montrealTripData = allDataArray[4];
+    let montrealMapData = allDataArray[5];
+    montrealBikeshareMapVis = new BikeshareMapVis('montreal-bikeshare-map-area', montrealStationData,
+        montrealTripData, montrealMapData);
 }
